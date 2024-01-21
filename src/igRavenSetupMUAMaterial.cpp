ac8dc72c8fca4af51d8958e16aed21c80b4f6707
nikita488 <6237881+nikita488@users.noreply.github.com>
2024-01-21 12:14:38 +0300

    Rewrote igRavenSetupMUAMaterial opt from scratch

--- src/igRavenSetupMUAMaterial.cpp
+++ src/igRavenSetupMUAMaterial.cpp
@@ -1,18 +1,86 @@
 #include "../include/igRavenSetupMUAMaterial.h"
 
-namespace Gap {
-
-namespace Opt {
-
-bool empty(igString str)
+namespace Gap
 {
-	return !str || strlen(str) <= 0;
+
+namespace Opt
+{
+
+igTextureBindAttr* findTextureBindAttr(igAttrSet* attrSet, igInt unitID = 0)
+{
+	for (igNode* currentNode = attrSet; currentNode->getParentCount() > 0; currentNode = currentNode->getParent(0))
+	{
+		if (currentNode->isOfType(igAttrSet::getClassMeta()))
+		{
+			attrSet = static_cast<igAttrSet*>(currentNode);
+
+			for (igInt i = 0; i < attrSet->getAttrCount(); i++)
+			{
+				igTextureBindAttr* textureBind = igTextureBindAttr::dynamicCast(attrSet->getAttr(i));
+
+				if (textureBind && textureBind->getUnitID() == unitID)
+					return textureBind;
+			}
+		}
+	}
+
+	return NULL;
+}
+
+igBool hasTextureBindAttr(igAttrSet* attrSet, igInt unitID = 0)
+{
+	return findTextureBindAttr(attrSet, unitID) != NULL;
+}
+
+igBool generateTangentsAndBinormals(igGeometry* geometry)
+{
+	for (igInt i = 0; i < geometry->getAttrCount(); i++)
+	{
+		igAttr* attr = geometry->getAttr(i);
+
+		if (attr->isOfType(igGeometryAttr::getClassMeta()))
+		{
+			igGeometryAttr* geometry = static_cast<igGeometryAttr*>(attr);
+			const igVertexFormat& format = geometry->getVertexFormat();
+
+			if (format.hasPositions() && format.hasNormals() && format.getTextureCoordCount() > 0 && !format.hasTangents() && !format.hasBinormals())
+			{
+				igVertexArrayHelperRef helper = igVertexArrayHelper::instantiateRef();
+				helper->generateTangentsAndBinormals(geometry, 0, true);
+				return true;
+			}
+		}
+		else if (attr->isOfType(igGeometryAttr2::getClassMeta()))
+		{
+			igGeometryAttr2* geometry = static_cast<igGeometryAttr2*>(attr);
+			igVertexArray2* vertices = geometry->getVertexArray();
+
+			if (vertices &&
+				vertices->findVertexData(igVertexData::IG_VERTEX_COMPONENT_POSITION) != NULL &&
+				vertices->findVertexData(igVertexData::IG_VERTEX_COMPONENT_NORMAL) != NULL &&
+				vertices->findVertexData(igVertexData::IG_VERTEX_COMPONENT_TEXCOORD) != NULL &&
+				vertices->findVertexData(igVertexData::IG_VERTEX_COMPONENT_TANGENT) == NULL &&
+				vertices->findVertexData(igVertexData::IG_VERTEX_COMPONENT_BINORMAL) == NULL)
+			{
+				Opt::igVertexTools::generateTangentsAndBinormals(geometry, 0, true);
+				return true;
+			}
+		}
+	}
+
+	return false;
 }
 
 igBool igRavenSetupMUAMaterial::configure(igInt sectionHandle)
 {
-	_regex->setRegularExpression(_diffuseMapName);
-	return !empty(_diffuseMapName);
+	if (_diffuseMapName != NULL && strlen(_diffuseMapName) > 0)
+	{
+		_regex->setRegularExpression(_diffuseMapName);
+		return true;
+	}
+
+	printf("The name of the Diffuse Map has not been set\n");
+	return false;
 }
 
 igMetaObject* igRavenSetupMUAMaterial::getVisitedObjectMeta()
@@ -20,75 +88,39 @@ igMetaObject* igRavenSetupMUAMaterial::getVisitedObjectMeta()
 	return igGeometry::getClassMeta();
 }
 
-igTextureBindAttr* findTextureAttribute(igAttrSet* attributes, igInt unitID = 0)
+igBool igRavenSetupMUAMaterial::addReflectionTextureMaps(igGeometry* geometry, igAttrSet* attrs)
 {
-    for (igInt i = 0; i < attributes->getAttrCount(); i++)
-    {
-        igTextureBindAttr* attribute = igTextureBindAttr::dynamicCast(attributes->getAttr(i));
-		
-        if (attribute && attribute->getUnitID() == unitID)
-            return attribute;
-    }
+	const igInt count = 6;
+	const igString faceTextures[count] = 
+	{
+		_reflectionMapRight, 
+		_reflectionMapLeft, 
+		_reflectionMapBack,
+		_reflectionMapFront, 
+		_reflectionMapUp, 
+		_reflectionMapDown
+	};
 
-    return NULL;
-}
+	for (igInt i = 0; i < count; i++)
+	{
+		igString faceTexture = faceTextures[i];
 
-igBool hasTextureAttribute(igAttrSet* attributes, igInt unitID = 0)
-{
-	return findTextureAttribute(attributes, unitID) != NULL;
-}
+		if (faceTexture == NULL || strlen(faceTexture) <= 0) {
+			return false;
+		}
+	}
 
-igImpStringIdRef igRavenSetupMUAMaterial::addTexture(igImpTexture texture)
-{
-	igImpStringIdRef id = igImpStringId::instantiateRefFromPool(getMemoryPool());
-	
-	id->setString(texture._name);
-	return _sceneGraph->getTextureMapPool()->addTextureMap(texture, id) ? id : NULL;
-}
-
-igTextureBindAttrRef igRavenSetupMUAMaterial::getTextureBind(igImpStringIdRef id, igInt unitID)
-{
-	igTextureBindAttr* textureBind = _sceneGraph->getTextureMapPool()->getTexBind(id);
-	
-	if (!textureBind)
-		return NULL;
-	
-	igTextureBindAttrRef copy = igTextureBindAttr::instantiateRefFromPool(kIGMemoryPoolAttribute);
-	copy->setTexture(textureBind->getTexture());
-	copy->setUnitID(unitID);
-	return copy;
-}
-
-void igRavenSetupMUAMaterial::appendTextureMap(igAttrSet* attributes, igImpStringIdRef id, igInt unitID)
-{
-	if (!id || hasTextureAttribute(attributes, unitID))
-		return;
-
-	igTextureBindAttrRef textureBind = getTextureBind(id, unitID);
-	igTextureStateAttr* textureState = _sceneGraph->getTextureMapPool()->getTextureEnable(unitID);
-
-	if (!textureBind || !textureState)
-		return;
-
-	attributes->appendAttr(textureBind);
-	attributes->appendAttr(textureState);
-}
-
-void igRavenSetupMUAMaterial::appendTextureMap(igAttrSet* attributes, igString texturePath, igInt unitID)
-{
-	appendTextureMap(attributes, addTexture(igImpTexture(texturePath)), unitID);
-}
-
-void igRavenSetupMUAMaterial::appendReflectionTextureMap(igAttrSet* attributes)
-{
-	if (hasTextureAttribute(attributes, 3))
-		return;
+	if (hasTextureBindAttr(geometry, 3))
+	{
+		printf("igTextureBindAttr with unitID 3 already exists on igGeometry `%s`\n", geometry->getName());
+		return false;
+	}
 
 	igImpStringId* textureID = _environmentMap->getTextureIdentifier();
 
 	if (!textureID)
 	{
-		const igInt count = 6;
+		igImpTextureMapBuilder* textureMaps = _sceneGraph->getTextureMapPool();
 		const igImpEnvironmentChannel::CubeFace cubeFaces[count] = 
 		{
 			igImpEnvironmentChannel::FACE_RIGHT,
@@ -98,156 +130,187 @@ void igRavenSetupMUAMaterial::appendReflectionTextureMap(igAttrSet* attributes)
 			igImpEnvironmentChannel::FACE_FRONT,
 			igImpEnvironmentChannel::FACE_BACK
 		};
-		const igString faceTextures[count] = 
-		{
-			_reflectionMapRight, 
-			_reflectionMapLeft, 
-			_reflectionMapBack,
-			_reflectionMapFront, 
-			_reflectionMapUp, 
-			_reflectionMapDown
-
-		};
 		const float faceRotations[count] = 
 		{
 			90.0F, 270.0F, 0.0F, 180.0F, 0.0F, 180.0F
 		};
-		igStringObjRef textureName = igStringObj::instantiateRefFromPool(kIGMemoryPoolTemporary);
+		igStringObjRef textureName = igStringObj::instantiateRef();
 
 		for (igInt i = 0; i < count; i++)
 		{
-			igImpTexture texture(faceTextures[i]);
+			igImpStringIdRef faceTextureID = igImpStringId::instantiateRef();
+
+			faceTextureID->setString(faceTextures[i]);
+			
+			if (!textureMaps->getTextureMap(faceTextureID))
+			{
+				const igString texturePath = faceTextureID->getString();
+				igImpTexture& texture = igImpTexture(texturePath);
 		
-			textureName->printf("%s.cube_%d", texture._fileName, i);
-			texture.setName(textureName->getBuffer());
-		
-			igImpStringIdRef textureID = addTexture(texture);
-		
-			if (textureID)
-				_environmentMap->setCubeFaceTexture(cubeFaces[i], textureID, faceRotations[i]);
+				textureName->printf("%s.cube_%d", texturePath, i);
+				texture.setName(textureName->getBuffer());
+
+				if (!textureMaps->addTextureMap(texture, faceTextureID))
+				{
+					printf("Failed to load texture image `%s` for cube face %d\n", texturePath, i);
+					return false;
+				}
+			}
+
+			_environmentMap->setCubeFaceTexture(cubeFaces[i], faceTextureID, faceRotations[i]);
 		}
 
 		_environmentMap->validate(_sceneGraph);
+		textureID = _environmentMap->getTextureIdentifier();
+		
+		if (textureID)
+		{
+			textureMaps->getTexBind(textureID)->setUnitID(3);
+		}
+		else
+		{
+			printf("Failed to create igTextureCubeAttr for igGeometry `%s`\n", geometry->getName());
+			return false;
+		}
 	}
 
-	appendTextureMap(attributes, textureID, 3);
+	printf("Adding igTextureBindAttr `%s` with unitID 3 to igGeometry `%s`\n", textureID->getString(), geometry->getName());
+	addTextureMap(textureID, attrs);
 
-	if (empty(_reflectionMaskMap))
+	if (!addTextureMap(geometry, _reflectionMaskMap, 4, attrs) && !hasTextureBindAttr(geometry, 4))
 	{
-		igShaderParametersAttrRef parameters = igShaderParametersAttr::instantiateRefFromPool(kIGMemoryPoolAttribute);
-		igShaderConstantVectorRef reflectance = igShaderConstantVector::instantiateRefFromPool(getMemoryPool());
+		printf("Adding igShaderParametersAttr with Reflectance %f to igGeometry `%s`\n", _reflectance, geometry->getName());
+		
+		igShaderParametersAttrRef parameters = igShaderParametersAttr::instantiateRef();
+		igShaderConstantVectorRef reflectance = igShaderConstantVector::instantiateRef();
 
 		reflectance->setName("material.reflectance");
 		reflectance->setData(igVec4f(_reflectance, 0.0F, 0.0F, 0.0F));
 		parameters->setUnitID(2);
 		parameters->getDataList()->append(reflectance);
-		attributes->appendAttr(parameters);
+		attrs->appendAttr(parameters);
 	}
-	else
-	{
-		appendTextureMap(attributes, _reflectionMaskMap, 4);
-	}
+
+	return true;
 }
 
-void generateTangentsAndBinormals(igGeometry* geometry)
+igBool igRavenSetupMUAMaterial::addTextureMap(igGeometry* geometry, igString texturePath, igInt unitID, igAttrSet* attrs)
 {
-	for (igInt i = 0; i < geometry->getAttrCount(); i++)
+	if (texturePath == NULL || strlen(texturePath) <= 0)
 	{
-		igAttr* attr = geometry->getAttr(i);
-		igGeometryAttr2* ga2 = igGeometryAttr2::dynamicCast(attr);
-		igGeometryAttr* ga = !ga2 ? igGeometryAttr::dynamicCast(attr) : NULL;
-		igVertexArray2* va2 = ga2 ? ga2->getVertexArray() : NULL;
-		const igVertexFormat* vf = !ga2 ? &ga->getVertexFormat() : NULL;
+		return false;
+	}
 
-		if (!ga2 && !ga)
-			continue;
+	if (unitID < 0 || unitID >= 8)
+	{
+		printf("Invalid unitID. Only unitID's 0..7 are supported\n");
+		return false;
+	}
 
-		if (ga2)
+	if (hasTextureBindAttr(geometry, unitID))
+	{
+		printf("igTextureBindAttr with unitID %d already exists on igGeometry `%s`\n", unitID, geometry->getName());
+		return false;
+	}
+
+	igImpTextureMapBuilder* textureMaps = _sceneGraph->getTextureMapPool();
+	igImpStringIdRef id = igImpStringId::instantiateRef();
+	
+	id->setString(texturePath);
+
+	if (!textureMaps->getTextureMap(id))
+	{
+		igImpTexture& texture = igImpTexture(texturePath);
+		
+		if (!textureMaps->addTextureMap(texture, id))
 		{
-			if (va2->findVertexData(igVertexData::IG_VERTEX_COMPONENT_TANGENT) && va2->findVertexData(igVertexData::IG_VERTEX_COMPONENT_BINORMAL))
-				continue;
-
-			if (!va2->findVertexData(igVertexData::IG_VERTEX_COMPONENT_POSITION) || !va2->findVertexData(igVertexData::IG_VERTEX_COMPONENT_TEXCOORD))
-				continue;
-
-			Opt::igVertexTools::generateTangentsAndBinormals(ga2, 0);
+			printf("Failed to load texture image `%s` for unitID %d\n", texturePath, unitID);
+			return false;
 		}
 		else
 		{
-			if (vf->hasTangents() && vf->hasBinormals())
-				continue;
-
-			if (!vf->hasPositions() || vf->getTextureCoordCount() <= 0)
-				continue;
-
-			igVertexArrayHelperRef helper = igVertexArrayHelper::instantiateRef();
-			helper->generateTangentsAndBinormals(ga, 0);
+			textureMaps->getTexBind(id)->setUnitID(unitID);
 		}
+	}
+
+	printf("Adding igTextureBindAttr `%s` with unitID %d to igGeometry `%s`\n", texturePath, unitID, geometry->getName());
+	addTextureMap(id, attrs);
+	return true;
+}
+
+void igRavenSetupMUAMaterial::addTextureMap(igImpStringId* textureID, igAttrSet* attrs)
+{
+	igImpTextureMapBuilder* textureMaps = _sceneGraph->getTextureMapPool();
+	igTextureBindAttr* textureBind = textureMaps->getTexBind(textureID);
+
+	if (textureBind)
+	{
+		attrs->appendAttr(textureBind);
+		attrs->appendAttr(textureMaps->getTextureEnable(textureBind->getUnitID()));
 	}
 }
 
-void igRavenSetupMUAMaterial::processGeometry(igGeometry* geometry, igAttrSet* attributes, igTextureBindAttr* diffuseTexture)
+igBool igRavenSetupMUAMaterial::addTextureMaps(igGeometry* geometry, igAttrSet* attrs)
 {
-	if (!diffuseTexture)
-		return;
+	igTextureBindAttr* diffuseTextureBind = findTextureBindAttr(geometry);
 
-	igStringObjRef textureName = igStringObj::instantiateRefFromPool(kIGMemoryPoolTemporary);
+	if (diffuseTextureBind == NULL)
+	{
+		printf("igTextureBindAttr with unitID 0 not found for igGeometry `%s`", geometry->getName());
+		return false;
+	}
 
-	textureName->set(diffuseTexture->getTexture()->getImage()->getName());
+	igStringObjRef textureName = igStringObj::instantiateRef();
+
+	textureName->set(diffuseTextureBind->getTexture()->getImage()->getName());
 	textureName->removePathFromFileName();
 	textureName->removeFileExtension();
 
 	if (!_regex->test(textureName->getBuffer()))
-		return;
+		return false;
 
-	if (!empty(_normalMap))
-	{
-		appendTextureMap(attributes, _normalMap, 1);
-		generateTangentsAndBinormals(geometry);
-	}
+	const igInt attrsCount = attrs->getAttrCount();
 
-	if (!empty(_specularMap))
-		appendTextureMap(attributes, _specularMap, 2);
+	if (addTextureMap(geometry, _normalMap, 1, attrs))
+		if (!generateTangentsAndBinormals(geometry))
+			printf("Failed to generate tangets and binormals for igGeometry `%s`\n", geometry->getName());
 
-	if (!empty(_reflectionMapRight) && !empty(_reflectionMapLeft) && !empty(_reflectionMapBack) && !empty(_reflectionMapFront) && !empty(_reflectionMapUp) && !empty(_reflectionMapDown))
-		appendReflectionTextureMap(attributes);
-
-	if (!empty(_emissiveMap))
-		appendTextureMap(attributes, _emissiveMap, 5);
+	addTextureMap(geometry, _specularMap, 2, attrs);
+	addReflectionTextureMaps(geometry, attrs);
+	addTextureMap(geometry, _emissiveMap, 5, attrs);
+	return attrs->getAttrCount() != attrsCount;
 }
 
 void igRavenSetupMUAMaterial::visitor(igObject* object)
 {
-	igGeometry* geometry = igGeometry::dynamicCast(object);
-	
-	if (!geometry)
-		return;
+	igGeometry* geometry = static_cast<igGeometry*>(object);
+	const igInt attrCount = geometry->getAttrCount();
+	const igInt parentCount = geometry->getParentCount();
 
-	int parentCount = geometry->getParentCount();
-
-	if (parentCount > 1)
+	if (parentCount == 1 && geometry->getParent(0)->isOfType(igAttrSet::getClassMeta()))
 	{
-		for (int i = 0; i < parentCount; i++)
-		{
-			igAttrSet* parent = igAttrSet::dynamicCast(geometry->getParent(i));
-			
-			if (!parent)
-				continue;
-			
-			igAttrSetRef attributes = igAttrSet::instantiateRefFromPool(kIGMemoryPoolAttribute);
+		igAttrSet* parentAttributes = static_cast<igAttrSet*>(geometry->getParent(0));
+		addTextureMaps(geometry, parentAttributes);
+	}
+	else
+	{
+		igAttrSetRef attributes = igAttrSet::instantiateRef();
 
-			processGeometry(geometry, attributes, findTextureAttribute(parent));
-			parent->appendChild(attributes);
-			parent->removeChild(geometry);
+		if (addTextureMaps(geometry, attributes))
+		{
+			for (igInt i = 0; i < parentCount; i++)
+			{
+				igGroup* parent = igGroup::dynamicCast(geometry->getParent(i));
+
+				if (parent)
+				{
+					parent->appendChild(attributes);
+					parent->removeChild(geometry);
+				}
+			}
+
 			attributes->appendChild(geometry);
 		}
-	}
-	else if (parentCount == 1)
-	{
-		igAttrSet* parent = igAttrSet::dynamicCast(geometry->getParent(0));
-
-		if (parent)
-			processGeometry(geometry, parent, findTextureAttribute(parent));
 	}
 }
 
